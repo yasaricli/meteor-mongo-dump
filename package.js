dd0e48ac197cd643da87abce72b031c2e9c3e5fe
Yasar icli <hello@yasaricli.com>
2016-08-03 16:29:45 +0300

    version changes

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'yasaricli:dump',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'In-app Backup for your Mongo Database',
   git: 'https://github.com/yasaricli/meteor-mongo-dump.git',
   documentation: 'README.md'
@@ -14,8 +14,8 @@ Npm.depends({
 
 Package.onUse(function(api) {
   api.use([
-    'ecmascript',
-    'underscore',
+    'ecmascript@0.4.6',
+    'underscore@1.0.9',
 
     'momentjs:moment@2.14.4'
   ]);
